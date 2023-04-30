d77a592253732494cc3d4361a8994885c5cbe8c6
Kazato Sugimoto <uiureo@gmail.com>
2023-04-30 13:47:03 +0900

    Add image_to_text.py script and update dependencies in poetry.lock
    The image_to_text.py script is a utility for detecting text in images using the Google Cloud Vision API. The script has two functions, detect_text() and detect_document(), each of which uses the Vision API to analyze an image and extract the text.
    
    In addition to the script, the poetry.lock file was updated to include new dependencies for the script to function properly. These dependencies include cachetools, google-api-core, google-auth, google

---
+++ image_to_text.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Usage: python image_to_text.py <path-to-image>
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    for text in texts:
+        print('{}'.format(text.description), end='')
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+def detect_document(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    print(response.full_text_annotation.text)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+if __name__ == '__main__':
+    import sys
+
+    # Use detect_document primarily
+    detect_document(sys.argv[1])
